8c7c131cb922ec242461317f6d15b7f930599af7
Devan C. M. Wijaya, S.Kom <54527592+devancakra@users.noreply.github.com>
2024-02-15 13:33:58 +0700

    Update WD1mini_bot.ino

--- Src/WD1mini_bot.ino
+++ Src/WD1mini_bot.ino
@@ -3,20 +3,24 @@ SoftwareSerial maps(12, 13); // D6 As RX, D7 As TX -> Wemos D1 Mini
 
 // Method: setup
 void setup() {
-  Serial.begin(9600); // start serial communication internally
-  maps.begin(9600); // start serial communication externally
-  while(!maps.available()){ Serial.println("Failed to connect, system is trying to reconnect !!"); } // serial communication with the Arduino Pro Micro board failed
+  Serial.begin(115200); // start serial communication internally
+  maps.begin(115200); // start serial communication externally  
 }
 
 // Method: loop
 void loop() {  
+  gpssensor(); // calling the gpsdata method
+}
+
+// Method: gpssensor
+void gpssensor(){
+  while(!maps.available()){ Serial.println("Failed to get sensor data, system tries to reconnect communication !!"); } // serial communication with the Arduino Pro Micro board failed
   Serial.println("Retrieve serial data from Arduino Pro Micro board..."); // display data to the Wemos D1 Mini serial monitor
-  delay(2000); // delay -> 2 second  
+  delay(2000); // delay -> 2 second 
   String location = ""; // this String data type is used to store location data
-  while(maps.available()){ // this loop is used to read the available location data from the serial object
-    location += char(maps.read()); // adds each character read into a data string named location
-  } 
-  Serial.print(location); // print location data on the serial monitor
-  location.trim(); // removes spaces at the beginning and end of string data
-  delay(1000); // delay -> 1 second
+  while(maps.available() > 0){ // this loop is used to read the available location data from the serial object
+    location += char(maps.read()); // adds each sensor data reading into a data string named raw_data
+  }  
+  Serial.println(location); // print location data on the serial monitor
+  delay(1000); // delay -> 1 second 
 }
