d95c8c2b5892e79ecada8c22e30dad0c75e0e24f
Devan C. M. Wijaya, S.Kom <54527592+devancakra@users.noreply.github.com>
2024-02-15 20:53:38 +0700

    Update WD1mini_bot.ino

--- Src/WD1mini_bot.ino
+++ Src/WD1mini_bot.ino
@@ -17,7 +17,7 @@ void loop() {
 // Method: gpssensor
 void gpssensor(){
   while(!mcuSerial.available()){ Serial.println("Failed to get sensor data, system tries to reconnect communication !!"); } // serial communication with the Arduino Pro Micro board failed
-  raw_data = ""; // this String data type is used to store latitude & longitude data
+  raw_data = ""; // this String data type is used to store data obtained from serial communication
   while(mcuSerial.available()){ // this loop is used to read the available location data from the serial object
     raw_data += char(mcuSerial.read()); // adds each sensor data reading into a data string named raw_data
   }  
