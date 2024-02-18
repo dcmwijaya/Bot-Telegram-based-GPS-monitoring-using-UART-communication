090a0e9fb7cef8a2704be48e2d8e93d703c5d7ae
Devan C. M. Wijaya, S.Kom <54527592+devancakra@users.noreply.github.com>
2024-02-18 14:20:01 +0700

    Update APmicro_gpstracker.ino

--- Src/APmicro_gpstracker.ino
+++ Src/APmicro_gpstracker.ino
@@ -24,31 +24,34 @@ void loop() {
 
 // Method: sensor
 void sensor(){
-  while (gpsSerial.available() > 0) // if there is serial communication from the GPS sensor then
-  gps.encode(gpsSerial.read()); // reading GPS data
-  if (gps.location.isValid()){ // if the GPS location is valid then
-    lat = (gps.location.lat()); // this float variable is to hold gps data -> latitude
-    lng = (gps.location.lng()); // this float variable is to hold gps data -> longitude
+  if(gpsSerial.available()){
+    while (gpsSerial.available()){ // if there is serial communication from the GPS sensor then
+      gps.encode(gpsSerial.read()); // reading GPS data
+      if (gps.location.isValid()){ // if the GPS location is valid then
+        lat = (gps.location.lat()); // this float variable is to hold gps data -> latitude
+        lng = (gps.location.lng()); // this float variable is to hold gps data -> longitude
+      }
+    }
+    // lat = -7.3364958; lng = 112.6367014; // dummy Data
+    latitude = String(lat, 6); // this string variable is to hold gps data -> latitude
+    longitude = String(lng, 6); // this string variable is to hold gps data -> longitude
+    delay(1000); // delay -> 1 second
+    view_data(); // calling the view_data method
+    send_data(); // calling the send_data method
   }
-  // lat = -7.3364958; lng = 112.6367014; // dummy Data
-  latitude = String(lat, 6); // this string variable is to hold gps data -> latitude
-  longitude = String(lng, 6); // this string variable is to hold gps data -> longitude
-  view_data(); // calling the view_data method
-  send_data(); // calling the send_data method
 }
 
 // Method: view_data
 void view_data(){
   // display data to the Arduino Pro Micro serial monitor
   Serial.println("Transmit serial data to Wemos D1 Mini...");
-  delay(2000); // delay -> 2 second
   Serial.println("latitude : " + latitude + "\nlongitude : " + longitude + "\n");
-  delay(1000); // delay -> 1 second
+  delay(5000); // delay -> 5 second
 }
 
 // Method: send_data
 void send_data(){
   // send data from Arduino Pro Micro to Wemos D1 Mini
   Serial1.println(latitude+","+longitude);
-  delay(1000); // delay -> 1 second
+  delay(5000); // delay -> 5 second
 }
