c8df437ac13154006d19a2b23c02544b44d73e09
devancakra <mudrawijaya@gmail.com>
2024-02-19 11:59:41 +0700

    Add: Component Testing

---
+++ Assets/Component Testing/SoftwareSerial_ArduinoProMicro.ino
@@ -0,0 +1,19 @@
+#include <SoftwareSerial.h>
+static const int Rx_pin = 9; // 9 As RX Arduino Pro Micro
+static const int Tx_pin = 8; // 8 As TX Arduino Pro Micro
+SoftwareSerial mySerial(Rx_pin,Tx_pin);
+void setup(){
+  Serial.begin(9600);
+  mySerial.begin(9600);
+}
+void loop(){
+  mySerial.print("hello Server");
+  delay(5000);
+
+  if(mySerial.available()> 0){
+    String data="";
+    while(mySerial.available()> 0){ data += char(mySerial.read()); }
+    delay(1000);
+    Serial.println(data);
+  }
+}
---
+++ Assets/Component Testing/SoftwareSerial_WemosD1Mini.ino
@@ -0,0 +1,19 @@
+#include <SoftwareSerial.h>
+static const int Rx_pin = 0; // D3 As RX Wemos D1 Mini
+static const int Tx_pin = 2; // D4 As TX Wemos D1 Mini
+SoftwareSerial mySerial(Rx_pin, Tx_pin);
+void setup(){
+  Serial.begin(9600);
+  mySerial.begin(9600);
+}
+void loop(){
+  mySerial.print("hello Client");
+  delay(5000);
+  
+  if(mySerial.available()> 0){
+    String data="";
+    while(mySerial.available()> 0){ data += char(mySerial.read()); }
+    delay(1000);
+    Serial.println(data);
+  }
+}
