507b03a432d1e71ec086febe95f4eb3923ca0feb
Devan C. M. Wijaya, S.Kom <54527592+devancakra@users.noreply.github.com>
2024-02-19 07:19:12 +0700

    Update WD1mini_bot.ino

--- Src/WD1mini_bot.ino
+++ Src/WD1mini_bot.ino
@@ -52,7 +52,7 @@ void gpssensor(){
 // function for data parsing
 String getValue(String data, char separator, int index){ // there are 3 parameters: data -> string, separator -> character, index -> integer
   int found = 0; // variable with integer type to store found data
-  int strIndex[] = {0, -1}; // variable with integer type to store two index values to be used as bounds when extracting substrings from the data string. The initial limit is 0, and the final limit is the final number read and then subtracted by 1.
+  int strIndex[] = {0, -1}; // variable with integer type to store two index values to be used as bounds when extracting substrings from the data string. the initial limit is 0, and the final limit is the final number read and then subtracted by 1
   int maxIndex = data.length()-1; // variable with integer type to store the upper limit of the index
  
   for(int i=0; i<=maxIndex && found<=index; i++){ // loop to search data
@@ -64,7 +64,7 @@ String getValue(String data, char separator, int index){ // there are 3 paramete
   } 
 
   // returns a value based on the condition:
-  // 1. checks whether the value found is greater than the index. If this condition is true, then the function will return the substring data starting from strIndex[0] to strIndex[1]
+  // 1. checks whether the value found is greater than the index. if this condition is true, then the function will return the substring data starting from strIndex[0] to strIndex[1]
   // 2. if the condition is false (meaning the found value is not greater than the index), then the function will return an empty string meaning no value was found at the requested index
   return found>index ? data.substring(strIndex[0], strIndex[1]) : "";
 }
@@ -91,7 +91,7 @@ void telegrambot(){
     TBMessage msg; // this variable to store telegram message data
   
     if(myBot.getNewMessage(msg)){ // if there is an incoming message...    
-      if(msg.messageType == CTBotMessageText){ // check what kind of message I received
+      if(msg.messageType == CTBotMessageText){ // checking the type of message received
         if(msg.text.equalsIgnoreCase("/start")){ // start
           sendMsg1 = "🙋🏻‍♂️ Hai @" + msg.sender.username + " 👋👋\nWelcome to Personal GPS Bot.\n\n";
           sendMsg1 += "‼️ Please input the secret code\n.................................. *(9 Characters)";
